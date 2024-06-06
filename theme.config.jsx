c36721cc4c09bbffd5e61e4f4a10e96f1b826557
utxo detective <harrison@beckerich.org>
2024-06-05 19:51:56 -0500

    update url

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,5 +1,6 @@
 export default {
 	logo: <span>Twetch Documentation</span>,
+	docsRepositoryBase: 'https://github.com/twetch-inc/docs',
 	project: {
 		link: 'https://github.com/twetch-inc/docs'
 	}
