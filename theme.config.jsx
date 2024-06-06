4a60e8d5a732aec168b4785dff64fc9f31609494
utxo detective <harrison@beckerich.org>
2024-06-05 19:55:03 -0500

    fix repo url

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,6 +1,6 @@
 export default {
 	logo: <span>Twetch Documentation</span>,
-	docsRepositoryBase: 'https://github.com/twetch-inc/docs',
+	docsRepositoryBase: 'https://github.com/twetch-inc/docs/blob/master',
 	project: {
 		link: 'https://github.com/twetch-inc/docs'
 	}
