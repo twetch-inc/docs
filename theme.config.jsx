7b872d1a9a98fd91a048ce3c14c7717cce706bdd
utxo detective <harrison@beckerich.org>
2024-06-05 20:25:47 -0500

    updates

--- theme.config.jsx
+++ theme.config.jsx
@@ -3,5 +3,16 @@ export default {
 	docsRepositoryBase: 'https://github.com/twetch-inc/docs/blob/master',
 	project: {
 		link: 'https://github.com/twetch-inc/docs'
-	}
+	},
+	useNextSeoProps() {
+		return {
+			titleTemplate: '%s – Twetch'
+		}
+	},
+	head: (
+		<>
+			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+			<meta property="og:image" content="https://twetch.com/unfurl.png" />
+		</>
+	)
 }
